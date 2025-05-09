cf4b63564176fda37faf7b4e5e252ebc10282697
Pablo_Herrera <pabloemi1998@gmail.com>
2025-05-09 16:58:23 -0500

    Agrega nuevas variables de entorno para la configuración de microservicios de productos y crea archivos de configuración para exportar las variables y servicios. Actualiza el DTO de órdenes para incluir un nuevo DTO de elementos de la orden y reorganiza las exportaciones.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -6,6 +6,8 @@ import { z } from 'zod';
 const envSchema = z.object({
     // NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
     PORT: z.coerce.number().int().positive(),
+    PRODUCTS_MICROSERVICES_HOST: z.string(),
+    PRODUCTS_MICROSERVICES_PORT: z.coerce.number().int().positive(),
 })
 
 // Luego haces el parsing:
@@ -18,5 +20,7 @@ if (!success) {
 
 export const envs = {
     PORT: data.PORT,
+    PRODUCTS_MICROSERVICES_HOST: data.PRODUCTS_MICROSERVICES_HOST,
+    PRODUCTS_MICROSERVICES_PORT: data.PRODUCTS_MICROSERVICES_PORT,
 }
 
